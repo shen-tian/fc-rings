dab9caba719b4dd597d4b32188aee3eb3065fd23
Shen Tian <biozinc@gmail.com>
2018-05-05 01:52:59 +0200

    Formatting

--- src/fc_rings/core.clj
+++ src/fc_rings/core.clj
@@ -6,23 +6,23 @@
 
 (set! *warn-on-reflection* true)
 
-(def width 192)
-(def height 192)
+(def width 128)
+(def height 64)
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
+                                        ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
+                                        ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb 100.0)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
+                                        ; setup function returns initial state. It contains
+                                        ; circle color and position.
   {:now 0
    :dx         0
    :dy         0
    :dz         0})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
+                                        ; Update sketch state by changing circle color and position.
   (let [now    (q/millis)
         speed  0.002
         zspeed 0.1
@@ -45,76 +45,67 @@
                                amp))
                        (/ amp 2)])
         start       [sx sy sz 0 1.0]
-        [_ _ _ r _] (nth (iterate iter-fn start) 4)
-        ]
+        [_ _ _ r _] (nth (iterate iter-fn start) 3)]
     r))
 
 (defn draw-state
   [{:keys [now dx dy dz] :as state}]
-  (let [^ints pixels     (q/pixels)
-        coords     (combo/cartesian-product (range (q/width)
-                                             )
-                                            (range (q/height)
-                                             ))
-        z          (* now 0.00008)
-        hue        (* now 0.01)
-        saturation (* 100
-                      (q/constrain
-                       (q/pow
-                        (* 1.15 (q/noise (* now 0.000122)))
-                        2.5)
-                       0 1))
-        scale      0.005
-        spacing    (* (- (q/noise (* now 0.000014))
-                         0.5)
-                      0.1)
-        centerx    (* (q/noise (* now 0.000125))
-                      1.25
-                      (q/width))
-        centery    (* (q/noise (* now -0.000125))
-                      1.25
-                      (q/height))]
+  (let [^ints pixels (q/pixels)
+        coords       (combo/cartesian-product (range (q/width))
+                                              (range (q/height)))
+        z            (* now 0.00008)
+        hue          (* now 0.01)
+        saturation   (* 100
+                        (q/constrain
+                         (q/pow
+                          (* 1.15 (q/noise (* now 0.000122)))
+                          2.5)
+                         0 1))
+        scale        0.005
+        spacing      (* (- (q/noise (* now 0.000014))
+                           0.5)
+                        0.1)
+        centerx      (* (q/noise (* now 0.000125))
+                        1.25
+                        (q/width))
+        centery      (* (q/noise (* now -0.000125))
+                        1.25
+                        (q/height))]
     (run!
      (fn [[x y]]
-       (let [dist  (q/sqrt (+ (q/pow (- x centerx) 2)
-                              (q/pow (- y centery) 2)))
-             pulse (* (- (q/sin (+ dz (* dist spacing)))
-                         0.3)
-                      0.3)
-             n     (- (fractal-noise (+ dx (* x scale) pulse)
-                                     (+ dy (* y scale))
-                                     z)
-                      0.75)
-             m     (- (fractal-noise (+ dx (* x scale))
-                                     (+ dy (* y scale))
-                                     (+ z 10.0))
-                      0.75)
-
+       (let [dist       (q/sqrt (+ (q/pow (- x centerx) 2)
+                                   (q/pow (- y centery) 2)))
+             pulse      (* (- (q/sin (+ dz (* dist spacing)))
+                              0.3)
+                           0.3)
+             n          (- (fractal-noise (+ dx (* x scale) pulse)
+                                          (+ dy (* y scale))
+                                          z)
+                           0.75)
+             m          (- (fractal-noise (+ dx (* x scale))
+                                          (+ dy (* y scale))
+                                          (+ z 10.0))
+                           0.75)
              ^int idx   (+ x (* y (q/width)))
              ^int color (q/color (mod (+ hue (* 40.0 m))
-                                 100)
-                            saturation
-                            (* 100.0
-                               (q/constrain (q/pow (* 3.0 n) 1.5)
-                                            0.0 0.9)))]
+                                      100)
+                                 saturation
+                                 (* 100.0
+                                    (q/constrain (q/pow (* 3.0 n) 1.5)
+                                                 0.0 0.9)))]
          (aset pixels idx
                color)))
      coords)
+    (println (q/current-frame-rate))
     (q/update-pixels)))
 
 (defn -main [& args]
   (q/defsketch fc-rings
-    :title "You spin my circle right round"
+    :title "Rings"
     :size [width height]
-    ;;:renderer :p2d
-    ; setup function called only once, during sketch initialization.
     :setup setup
-    ; update-state is called on each iteration before draw-state.
     :update update-state
     :draw draw-state
     :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
     :middleware [m/pause-on-error
                  m/fun-mode]))
